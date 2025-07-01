3c8648cba42198dce9bf4c3a7f5869eaf0f41227
fgqerfgq <mpg928.pc@gmail.com>
2025-07-02 04:42:58 +0530

    Delete src/index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import FundingLayer from "./App";
-import "./index.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FundingLayer />);
